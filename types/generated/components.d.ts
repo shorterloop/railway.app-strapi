7adeacf2fd8f1739b12c34d216a805cf1602ce8e
DineshCodeFlow <drawat@shorterloop.com>
2025-04-26 07:48:56 +0530

    Added call to action

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,29 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SharedCallToAction extends Struct.ComponentSchema {
+  collectionName: 'components_shared_call_to_actions';
+  info: {
+    displayName: 'Call to Action';
+    icon: 'emotionHappy';
+  };
+  attributes: {
+    Label: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'Start Free Trial'>;
+    URL: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'https://app.shorterloop.com/register'>;
+    Variant: Schema.Attribute.Enumeration<
+      ['primary', 'secondary', 'outline', 'ghost']
+    > &
+      Schema.Attribute.DefaultTo<'primary'>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'shared.call-to-action': SharedCallToAction;
+    }
+  }
+}
